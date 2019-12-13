f11de237f523cbd57dd87bd00a4d6a8c39bb6994
koko <kornel.kovacs@enet.hu>
2019-12-13 14:28:44 +0100

    updated VIE scraper

--- VIE_scraper.py
+++ VIE_scraper.py
@@ -14,11 +14,8 @@ headers = {
     'Accept-Language': 'en-GB,en;q=0.9,hu-HU;q=0.8,hu;q=0.7,en-US;q=0.6',
 }
 
-params = (
-    ('dummy', 'R08452f1Xc2dbb0c'),
-)
 ### Departure data
-response = requests.get('https://www.viennaairport.com/jart/prj3/va/data/flights/out.json', headers=headers, params=params)
+response = requests.get('https://www.viennaairport.com/jart/prj3/va/data/flights/out.json', headers=headers)
 df = pd.DataFrame(response.json()["monitor"]["departure"])
 
 df["status"] = [status["description"] for status in df["status"]]
@@ -30,7 +27,7 @@ df.drop(columns = ["checkin", "codeshares", "idx", "gate", "gateArea"], inplace 
 df_dep = df.loc[df["status"] == "airborne"]
 
 ### Arrival data
-response = requests.get('https://www.viennaairport.com/jart/prj3/va/data/flights/inc.json', headers=headers, params=params)
+response = requests.get('https://www.viennaairport.com/jart/prj3/va/data/flights/inc.json', headers=headers)
 df = pd.DataFrame(response.json()["monitor"]["departure"])
 
 df["status"] = [status["code"] for status in df["status"]]
@@ -44,6 +41,7 @@ df_arr = df.loc[df["status"] == "BLI"]
 
 df_final = pd.concat([df_arr, df_dep], sort = False).reset_index(drop = True)
 
-print(len(df_final))
+print("Airborne: ", len(df_dep))
+print("Arrival: ", len(df_arr))
 stamp = datetime.today().strftime("%Y_%m_%d_%H_%M")
 df_final.to_pickle(f".\\data\\VIE_{stamp}.pkl")
